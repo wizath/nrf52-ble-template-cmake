6bde9fc914ac3bdd03b6448259b6e0246a70f132
wizath <kubajot93@gmail.com>
2021-09-26 21:36:51 +0200

    Initial

---
+++ sdk/external/micro-ecc/nrf52hf_armgcc/armgcc/_build/micro_ecc_lib/uECC.c.d
@@ -0,0 +1,27 @@
+_build/micro_ecc_lib/uECC.c.o: ../../micro-ecc/uECC.c \
+ ../../micro-ecc/uECC.h \
+ /Applications/ARM/lib/gcc/arm-none-eabi/10.2.1/include/stdint.h \
+ /Applications/ARM/arm-none-eabi/include/stdint.h \
+ /Applications/ARM/arm-none-eabi/include/machine/_default_types.h \
+ /Applications/ARM/arm-none-eabi/include/sys/features.h \
+ /Applications/ARM/arm-none-eabi/include/_newlib_version.h \
+ /Applications/ARM/arm-none-eabi/include/sys/_intsup.h \
+ /Applications/ARM/arm-none-eabi/include/sys/_stdint.h \
+ ../../micro-ecc/uECC_vli.h ../../micro-ecc/types.h \
+ ../../micro-ecc/platform-specific.inc ../../micro-ecc/asm_arm.inc \
+ ../../micro-ecc/asm_arm_mult_square_umaal.inc \
+ ../../micro-ecc/curve-specific.inc
+../../micro-ecc/uECC.h:
+/Applications/ARM/lib/gcc/arm-none-eabi/10.2.1/include/stdint.h:
+/Applications/ARM/arm-none-eabi/include/stdint.h:
+/Applications/ARM/arm-none-eabi/include/machine/_default_types.h:
+/Applications/ARM/arm-none-eabi/include/sys/features.h:
+/Applications/ARM/arm-none-eabi/include/_newlib_version.h:
+/Applications/ARM/arm-none-eabi/include/sys/_intsup.h:
+/Applications/ARM/arm-none-eabi/include/sys/_stdint.h:
+../../micro-ecc/uECC_vli.h:
+../../micro-ecc/types.h:
+../../micro-ecc/platform-specific.inc:
+../../micro-ecc/asm_arm.inc:
+../../micro-ecc/asm_arm_mult_square_umaal.inc:
+../../micro-ecc/curve-specific.inc:
